0df3fbf05352399ce21f8a78fea621042a849f31
Lei Jiang <ycarusskywalker@outlook.com>
2016-04-03 14:59:46 +0800

    modify2

--- Task19/task19.js
+++ Task19/task19.js
@@ -3,34 +3,53 @@
  */
 (function () {
     var input = document.getElementById("input");
-    var bts = document.getElementsByClassName("buttons");
     var timeInterval = document.getElementById("timeInterval");
     var dataArray = [];
     var display = document.getElementById("display");
     var dataReg = /^\d+$/;
-    $ = function (selector) {
+    var inAnimation = false;
+    var time_Interval = 300;
+    var $ = function (selector) {
         return document.querySelector(selector);
     };
+    var $$ = function (selector) {
+        return document.querySelectorAll(selector);
+    };
+    var animateQueue = delay(function () {
+    }, 0);
     $("#leftIn").onclick = function () {
+        if (inAnimation)return alert("In Animation");
         verifyInput(input, "left");
     };
     $("#rightIn").onclick = function () {
+        if (inAnimation)return alert("In Animation");
         verifyInput(input, "right");
     };
     $("#leftOut").onclick = function () {
+        if (inAnimation)return alert("In Animation");
         divOut(input, "left");
     };
     $("#rightOut").onclick = function () {
+        if (inAnimation)return alert("In Animation");
         divOut(input, "right");
     };
     $("#bubblingSort").onclick = function () {
+        if (inAnimation)return alert("In Animation");
         dataArray = bubblingSort(dataArray);
     };
     $("#quickSort").onclick = function () {
-        dataArray = quickSort(dataArray);
-        renderAll(dataArray);
+        if (inAnimation)return alert("In Animation");
+        getTimeInterval();
+        inAnimation = true;
+        dataArray = sort(0, dataArray.length - 1, dataArray);
+        animateQueue.delay(function () {
+            inAnimation = false;
+        })
     };
-    $("#random50").onclick = initial;
+    $("#random50").onclick = function () {
+        if (inAnimation)return alert("In Animation");
+        initial();
+    };
     delegateEvent(display, "div", "click", clickToRemove);
     initial();
     function verifyInput(inputValue, direction) {
@@ -97,6 +116,7 @@
     function delegateEvent(delegateElement, targetElement, eventName, handler) {
         delegateElement.addEventListener(eventName, function (event) {
             if (event.target.nodeName.toLowerCase() === targetElement.toLowerCase()) {
+                if (inAnimation)return alert("In Animation");
                 return handler(event);
             }
         }, false);
@@ -119,15 +139,17 @@
         item.setAttribute("style", "height:" + value + "px;top:" + (100 - value) + "px;line-height:" + value + "px");
     }
 
-    function bubblingSort(array) {
-        var time_Interval = timeInterval.value.trim();
+    function getTimeInterval() {
+        time_Interval = timeInterval.value.trim();
         if (time_Interval.search(dataReg) + 1 === 0) {
             alert("Please Input Valid Time Interval");
-            return array;
         } else {
             time_Interval = +time_Interval;
         }
+    }
 
+    function bubblingSort(array) {
+        getTimeInterval();
         for (var i = 0; i < array.length; i++) {
             (function (i) {
                 setTimeout(function () {
@@ -147,37 +169,71 @@
         return array;
     }
 
-    function quickSort(array) {
-        var lastEnd = 0;
-
-        function sort(firstIndex, arrayLength) {
-            var i = firstIndex;
-            var j = arrayLength - 1;
-            var flag = array[i];
-            if ((arrayLength - i) > 1) {
-                while (i < j) {
-                    for (; i < j; j--) {
-                        if (array[j] < flag) {
-                            array[i++] = array[j];
-                            break;
-                        }
-                    }
-                    for (; i < j; i++) {
-                        if (array[i] > flag) {
-                            array[j--] = array[i];
-                            break;
-                        }
-                    }
+    function sortPartition(left, right, array) {
+        var flag = array[left];
+        rangeRender(left, right);
+        while (left < right) {
+            animateRender(left);
+            for (; left < right; right--) {
+                animateRender(right);
+                if (array[right] < flag) {
+                    array[left] = array[right];
+                    animateRender(left, array[left]);
+                    left++;
+                    break;
                 }
-                array[i] = flag;
-                sort(lastEnd, i);
-                lastEnd = i + 1;
-                sort(i + 1, arrayLength);
+            }
+            for (; left < right; left++) {
+                animateRender(left);
+                if (array[left] > flag) {
+                    array[right] = array[left];
+                    animateRender(right, array[right]);
+                    right--;
+                    break;
+                }
             }
         }
+        array[left] = flag;
+        rangeRender(left, right);
+        animateRender(left, array[left]);
+        return left;
+    }
 
-        sort(0, array.length);
+    function sort(left, right, array) {
+        var index = sortPartition(left, right, array);
+        if ((index - left) > 1) {
+            sort(left, index, array);
+        }
+        if ((right - index) > 1) {
+            sort(index + 1, right, array);
+        }
         return array;
+    }
+
+    function animateRender(index, value) {
+        animateQueue.delay(function () {
+            $$("#display div")[index].style.backgroundColor = "chocolate";
+            $$("#display div")[index].style.color = "white";
+
+        }, 0);
+        animateQueue.delay(function () {
+            if (value) {
+                $$("#display div")[index].setAttribute("style", "height:" + value * 4 + "px;top:" + (100 - value * 4) + "px;line-height:" + value * 4 + "px");
+                $$("#display div")[index].innerText = value;
+            }
+        }, 0);
+        animateQueue.delay(function () {
+            $$("#display div")[index].style.backgroundColor = "white";
+            $$("#display div")[index].style.color = "chocolate";
+        }, time_Interval);
+    }
+
+    function rangeRender(left, right) {
+        animateQueue.delay(function () {
+            for (; left <= right; left++) {
+                $$("#display div")[left].style.backgroundColor = "wheat";
+            }
+        })
     }
 
     function renderAll(array) {
@@ -190,5 +246,36 @@
             fragment.appendChild(newDiv);
         }
         display.appendChild(fragment);
+    }
+
+    function delay(fn, t) {
+        var queue = [], self, timer;
+
+        function schedule(fn, t) {
+            timer = setTimeout(function () {
+                timer = null;
+                fn();
+                if (queue.length) {
+                    var next = queue.shift();
+                    schedule(next.fn, next.t);
+                }
+            }, t)
+        }
+
+        self = {
+            delay: function (fn, t) {
+                if (queue.length || timer) {
+                    queue.push({fn: fn, t: t})
+                } else {
+                    schedule(fn, t);
+                }
+                return self;
+            },
+            cancel: function () {
+                clearTimeout(timer);
+                queue = [];
+            }
+        };
+        return self.delay(fn, t);
     }
 })();
